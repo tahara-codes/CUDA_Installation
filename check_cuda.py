5a140ac29f20415f19e32774e2c4dec2122d6732
tahara-study <tahara.study@gmail.com>
2022-05-15 22:33:18 +0900

    Add test

---
+++ check_cuda.py
@@ -0,0 +1,21 @@
+import torch
+
+print(torch.__version__)
+
+print(torch.cuda.is_available())
+
+print(torch.cuda.device_count())
+
+print(torch.cuda.current_device())
+
+print(torch.cuda.get_device_name())
+
+print(torch.cuda.get_device_capability())
+
+print(torch.cuda.get_device_name(torch.device("cuda:0")))
+
+array = torch.zeros(4)
+print(array)
+
+array_gpu = array.cuda()
+print(array_gpu)
